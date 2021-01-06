e2ce88e5db2fde100e78f263649a8e55685fe3e4
AIhimik <alhimikpay@gmail.com>
2021-01-06 17:35:48 +0300

    Первая задача work1.py

---
+++ work1.py
@@ -0,0 +1,10 @@
+print('Введите число a')
+a = float(input())
+print('Введите число b')
+b = float(input())
+summa = a + b
+raznost = a - b
+proizvedenie = a * b
+print('Сумма =', summa)
+print('Разность =', raznost)
+print('Произведение =', proizvedenie)
